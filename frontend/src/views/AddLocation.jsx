86954b745cbbec57418ef301ecd9c371efd35883
Muhammad Haroon Arshad <haroon_ars@hotmail.com>
2019-07-04 01:21:45 +0200

    - Created Add-Location module - Added routes for Add-Location - Added view for Add-Location - Added css for Add-Location

---
+++ frontend/src/views/AddLocation.jsx
@@ -0,0 +1,191 @@
+import React, {Component} from "react";
+import {
+    Grid,
+    Row,
+    Col,
+    FormGroup,
+    ControlLabel,
+    FormControl
+} from "react-bootstrap";
+
+import {Card} from "components/Card/Card.jsx";
+import {FormInputs} from "components/FormInputs/FormInputs.jsx";
+import {UserCard} from "components/UserCard/UserCard.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+
+import avatar from "assets/img/faces/face-3.jpg";
+
+class AddLocation extends Component {
+    msg = ["Charging Unit 1","Charging Unit 2","Charging Unit 3"];
+
+    render() {
+        return (
+            <div className="content">
+                <Grid fluid>
+                    <Row>
+                        <Col md={6}>
+                            <Card
+                                title="Charging Location"
+                                content={
+                                    <form>
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "Location Name",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Location Name",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "Street",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Street",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+                                        <FormInputs
+                                            ncols={["col-md-4", "col-md-4", "col-md-4"]}
+                                            properties={[
+                                                {
+                                                    label: "City",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "City",
+                                                    defaultValue: ""
+                                                },
+                                                {
+                                                    label: "Country",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Country",
+                                                    defaultValue: ""
+                                                },
+                                                {
+                                                    label: "Postal Code",
+                                                    type: "number",
+                                                    bsClass: "form-control",
+                                                    placeholder: "ZIP Code"
+                                                }
+                                            ]}
+                                        />
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "State",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "State",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "Country",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Country",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "Cancellation Timeout (Minutes)",
+                                                    type: "number",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Cancellation Timeout",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+
+                                        <Row>
+                                            <Col md={12}>
+                                                <label>Charging Units</label>
+                                                <ul>
+                                                    {this.msg.map((value, index) => {
+                                                        return <li key={index}>{value}</li>
+                                                    })}
+                                                </ul>
+                                            </Col>
+                                        </Row>
+                                        <Button bsStyle="info" pullRight fill type="submit">
+                                            Add Charging Location
+                                        </Button>
+                                        <div className="clearfix"/>
+                                    </form>
+                                }
+                            />
+                        </Col>
+
+                        <Col md={6}>
+                            <Card
+                                title="Add/Edit Charging Unit"
+                                content={
+                                    <form>
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "Charger Name",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Charger Name",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+                                        <label htmlFor="chargerType">Type of charger</label>
+                                        <select className="form-control" id="chargerType">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                        <FormInputs
+                                            ncols={["col-md-12"]}
+                                            properties={[
+                                                {
+                                                    label: "Cost per kWh",
+                                                    type: "text",
+                                                    bsClass: "form-control",
+                                                    placeholder: "Street",
+                                                    defaultValue: ""
+                                                }
+                                            ]}
+                                        />
+                                        <Button bsStyle="danger" pullRight fill type="cancel">
+                                            Cancel
+                                        </Button>
+                                        <Button bsStyle="info form-save" pullRight fill type="submit">
+                                            Add/Update Charging unit
+                                        </Button>
+                                        <div className="clearfix"/>
+                                    </form>
+                                }
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default AddLocation;
